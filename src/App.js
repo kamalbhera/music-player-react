ee96307b19474040d5a4b2e378b384e5f40bbd34
Kamal Bhera <kamalbhera96@gmail.com>
2022-12-23 23:30:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Player from "@madzadev/audio-player";
+import "@madzadev/audio-player/dist/index.css";
+const tracks = [
+  {
+    url: "https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3",
+    title: "Madza - Chords of Life",
+    tags: ["house"],
+  },
+  {
+    url: "https://audioplayer.madza.dev/Madza-Late_Night_Drive.mp3",
+    title: "Madza - Late Night Drive",
+    tags: ["dnb"],
+  },
+  {
+    url: "https://audioplayer.madza.dev/Madza-Persistence.mp3",
+    title: "Madza - Persistence",
+    tags: ["dubstep"],
+  },
+];
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Music <code>Player App</code> React.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='player'>
+          <Player trackList={tracks} />
+        </div>
+       
       </header>
     </div>
   );
